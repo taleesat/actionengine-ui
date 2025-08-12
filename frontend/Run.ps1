662d1bccbe710972aee99a468fd7b8fa3de35b56
Tanakorn Leesatapornwongsa <taleesat@microsoft.com>
2025-08-11 23:56:51 -0700

    Open to all addresses

--- frontend/Run.ps1
+++ frontend/Run.ps1
@@ -6,4 +6,4 @@ $env:AZURE_OPENAI_TEXT_MODEL = "gpt-4o"
 #$env:DOCKER_START_PORT = "9800"
 $env:MULTI_PLAYWRIGHT_SERVER_ADDRESS = "4.155.4.13"
 $env:MULTI_PLAYWRIGHT_SERVER_PORT = "3000"
-magentic ui --port 8081 --config config.yaml
+magentic ui --host 0.0.0.0 --port 8081 --config config.yaml
