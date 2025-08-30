36de4a3e57b23715db7a47249777226a5a1c794a
Tanakorn Leesatapornwongsa <taleesat@microsoft.com>
2025-08-29 17:03:09 -0700

    Update run script

--- frontend/Run.ps1
+++ frontend/Run.ps1
@@ -1,9 +1,6 @@
 $env:AZURE_OPENAI_ENDPOINT = "https://actionengine-aoai-west.openai.azure.com/"
 $env:AZURE_OPENAI_API_VERSION = "2025-03-01-preview"
-$env:AZURE_OPENAI_TEXT_MODEL = "gpt-4o"
-#$env:DOCKER_ADDRESS = "4.155.4.13"
-#$env:DOCKER_PORT = "4375"
-#$env:DOCKER_START_PORT = "9800"
+$env:AZURE_OPENAI_TEXT_MODEL = "azure/gpt-4o-2"
 $env:MULTI_PLAYWRIGHT_SERVER_ADDRESS = "4.155.4.13"
 $env:MULTI_PLAYWRIGHT_SERVER_PORT = "3000"
 magentic ui --host 0.0.0.0 --port 8082 --config config.yaml
