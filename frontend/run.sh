e4e1e2a3bf21b11bbe4a9b73c0944da2a5127153
Tanakorn Leesatapornwongsa <taleesat@microsoft.com>
2025-09-30 16:52:52 -0700

    Add comments in run.sh

--- frontend/run.sh
+++ frontend/run.sh
@@ -5,7 +5,7 @@ export AZURE_OPENAI_API_VERSION="2025-03-01-preview"
 export AZURE_OPENAI_TEXT_MODEL="azure/gpt-4o-2"
 export AZURE_SCOPE="https://cognitiveservices.azure.com/.default"
 export DEPLOYMENT="local"
-export MULTI_PLAYWRIGHT_SERVER_ADDRESS="localhost"
-export MULTI_PLAYWRIGHT_SERVER_PORT="3000"
-export MCPSTUDIO_UI_PORT="9000"
+export MULTI_PLAYWRIGHT_SERVER_ADDRESS="localhost" # IP address of the browser server
+export MULTI_PLAYWRIGHT_SERVER_PORT="3000" # Port of the browser server
+export MCPSTUDIO_UI_PORT="9000" # Port to be opend
 magentic ui --host 0.0.0.0 --port $MCPSTUDIO_UI_PORT --config config.yaml
