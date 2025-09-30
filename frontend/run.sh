074c5053593d235708cd2efc382bae946cb532c1
Tanakorn L <taleesat@microsoft.com>
2025-09-30 16:45:00 -0700

    Add build.txt and edit run.sh

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
 
+export AZURE_OPENAI_ENDPOINT="https://actionengine-aoai-west.openai.azure.com/"
+export AZURE_OPENAI_API_VERSION="2025-03-01-preview"
+export AZURE_OPENAI_TEXT_MODEL="azure/gpt-4o-2"
+export AZURE_SCOPE="https://cognitiveservices.azure.com/.default"
+export DEPLOYMENT="local"
+export MULTI_PLAYWRIGHT_SERVER_ADDRESS="localhost"
+export MULTI_PLAYWRIGHT_SERVER_PORT="3000"
+export MCPSTUDIO_UI_PORT="9000"
 magentic ui --host 0.0.0.0 --port $MCPSTUDIO_UI_PORT --config config.yaml
